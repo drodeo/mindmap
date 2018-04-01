3e8415c071054ee091f2ac858e4086d4586afc5e
DmitryR <paukov2967@gmail.com>
2018-04-01 12:06:56 +0300

    initial

---
+++ app/javascript/app.vue
@@ -0,0 +1,23 @@
+
+   <div id='hello'>
+     {{message}}
+     <app></app>
+   </div>
+
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
